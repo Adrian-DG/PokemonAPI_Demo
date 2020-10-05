1f1e46d2424a09de65bd94331453ccc81706b871
Adrian-DG <adrian96guilamo@gmail.com>
2020-10-04 22:12:19 -0400

    get_more() method pending

--- Main.js
+++ Main.js
@@ -4,20 +4,19 @@ const POKEMON_URL = 'pokemon/:id'
 const pokedex = document.getElementById('poke-list')
 const moreBtn = document.getElementById('more-btn')
 
-const ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14]
+const ids = [1, 2, 3, 4, 5, 6, 7]
 let ids_len = ids.length
 
-moreBtn.onclick = function(){
-    const array = []
-    for(let i=1;i<=7;i++){
-        array.push(ids_len+i) 
+moreBtn.addEventListener('click', async () => {
+    let array = []
+    for(let i=1; i<7; i++){
+        array.push(i+ids_len)
+        ids_len += i
     }
-    
-    ids_len += 7
-
-    console.log(array)
+   
     fillPokedex(array)
-}
+   
+})
 
 // Renders the pokemon card info to pokedex list (<ul>)
 function render_html(pokemon_card) {
@@ -29,31 +28,29 @@ function render_html(pokemon_card) {
 
 // Verify if a pokemon has more than one type or element
 function checkTypes(params) {
-    return params[1] ? `<li>${params[1].type.name}</li>` : ''
+    return params[1] ? `<li>${params[1].type.name}</li>` : '' // if/else ternary operator 
+}
+
+// Change pokemon name to first letter uppercase
+function changeName(pokemon_name){
+    let name = Array.from(pokemon_name) // creates an array from (pokemon_name) letters
+    name[0] = name[0].toUpperCase() // set name array first letter to UpperCase()
+    return name.join('',).toString()
 }
 
 // Estructure the pokemon info to html tags 
 function to_html(pokemon) {
-
+    console.log(pokemon.name)
     const pokemon_card = 
     `
     <div class="card">
-        <img src="${pokemon.sprites.back_default}" alt="" styles="" class="poke-img card-img-top">
+        <img src="${pokemon.sprites.front_default}" alt="" styles="" class="poke-img card-img-top">
         <div class="card-body">
-            <h5 class="card-title text-center">${pokemon.name}</h5>
-            <div class="types">
+            <h5 class="card-title text-center">${changeName(pokemon.name)}</h5>
             <ul class="card-text">
-            <li>${pokemon.types[0].type.name}</li>
-            ${ checkTypes(pokemon.types) }
-        </ul>
-            </div>
-            <div class="more">
-                <div class="col-md-8"></div>
-                <div class="col-md-4">
-                    <button class="btn btn-secondary">+</button>
-                </div>
-            </div>
-
+                <li>${pokemon.types[0].type.name}</li>
+                ${ checkTypes(pokemon.types) /* check if pokemon has more than one type */ } 
+            </ul>
         </div>
     </div>
     `
@@ -63,24 +60,18 @@ function to_html(pokemon) {
 // fetch pokemon object from API
 async function fetchPokemon(id) {
     const poke_promise = await fetch(`${API_URL}${POKEMON_URL.replace(':id', id)}`)
+    // console.log(poke_promise)
     return poke_promise.json()
 }
 
 async function fillPokedex(array) {
     const pokemon_promises = array.map(id => fetchPokemon(id)) // request each pokemon by ID
     const pokemon_list = await Promise.all(pokemon_promises) 
+    console.log(pokemon_list)
     return pokemon_list
 }
 
-
 // Once the operation has been completed 
 fillPokedex(ids)
-    .then(data => {
-        data.forEach(item => {
-            // console.log(item.name)
-            to_html(item)
-        })
-    })
-    .catch(error => {
-        console.log(error)
-    })
+    .then(data => data.forEach(item => to_html(item)))
+    .catch(error => console.log(error))
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PokemonAPI_Demo
+
+In this project I make use of the Pokemon API.
